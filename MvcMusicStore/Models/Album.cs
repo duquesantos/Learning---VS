2e3c5fc8d8925fd49a94fcc8016157b0bc61ad7b
Luís Duque Santos <lmd.santos@campus.fct.unl.pt>
2015-03-09 17:43:13 +0000

    For the win!

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string AlbumName { get; set; }
+
+    }
+}
---
+++ MvcMusicStore/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        public string ReviewerEmail { get; set; }
+    }
+}
